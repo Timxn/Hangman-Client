92582d4fcd445f03dd08a5ce6466e97cfad82f92
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-07-03 23:34:00 +0200

    add json/rename application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module de.oose.gameservice.gameservice_client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
-    opens de.oose.gameservice.gameservice_client to javafx.fxml;
     exports de.oose.gameservice.gameservice_client;
     exports de.oose.gameservice.api;
-    opens de.oose.gameservice.api to javafx.fxml;
+    opens de.oose.gameservice.gameservice_client to javafx.fxml;
 }
