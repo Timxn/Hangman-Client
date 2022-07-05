99c530a493fc8be5a1da3d98b1e8ca200fbce1f7
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-07-05 12:41:44 +0200

    basic implementation

--- src/main/java/de/oose/gameservice/gameservice_client/AfterGameController.java
+++ src/main/java/de/oose/gameservice/gameservice_client/AfterGameController.java
@@ -1,4 +1,14 @@
 package de.oose.gameservice.gameservice_client;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
 public class AfterGameController {
+    private void update() {}
+    public void initialize() {
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(60), e ->update()));
+        tl.setCycleCount(Timeline.INDEFINITE);
+        tl.play();
+    }
 }
