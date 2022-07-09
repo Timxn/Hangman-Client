8236bd611f1d434c0ff2f395af30b328b81a51be
Justus Pfaue <s6jupfau@uni-bonn.de>
2022-07-09 13:22:47 +0200

    implement win/lose screen

--- src/main/java/de/oose/gameservice/gameservice_client/AfterGameController.java
+++ src/main/java/de/oose/gameservice/gameservice_client/AfterGameController.java
@@ -3,26 +3,63 @@ package de.oose.gameservice.gameservice_client;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
 import static java.lang.System.exit;
+import static java.lang.System.out;
 
 public class AfterGameController {
+    Timeline tl;
     @FXML
     Label output_result;
     @FXML
     Button button_restart, button_quit;
-    private void update() {}
+    private void update() {
+        try {
+            if (ClientApplication.api.isStarted()) {
+                // refactor this later
+                tl.stop();
+                FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("Game.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 1002, 699);
+                ClientApplication.stage.setScene(scene);
+                ClientApplication.stage.show();
+            }
+        } catch (Exception e) {
+            output_result.setText(e.getMessage());
+        }
+    }
+    public void restart() {
+        try {
+            ClientApplication.api.startGame();
 
-    public void restart() {}
-    public void quit() {exit(0);}
+        } catch (Exception e) {
+            output_result.setText(e.getMessage());
+        }
+    }
+    public void quit() {
+        try {
+            ClientApplication.api.close();
+
+            // refactor this later
+            tl.stop();
+            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("Game.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1002, 699);
+            ClientApplication.stage.setScene(scene);
+            ClientApplication.stage.show();
+        } catch (Exception e) {
+            output_result.setText(e.getMessage());
+        }
+        exit(0);
+    }
     public void initialize() {
         if (ClientApplication.api.won) output_result.setText("You won");
         else output_result.setText("You lost");
-//        Timeline tl = new Timeline(new KeyFrame(Duration.millis(60), e ->update()));
-//        tl.setCycleCount(Timeline.INDEFINITE);
-//        tl.play();
+        tl = new Timeline(new KeyFrame(Duration.millis(500), e ->update()));
+        tl.setCycleCount(Timeline.INDEFINITE);
+        tl.play();
     }
 }
