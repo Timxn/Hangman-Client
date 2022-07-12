ea8211bc827e1446b1c7d4651fcfa738cc87ce53
justus.pfaue <s6jupfau@uni-bonn.de>
2022-07-12 11:41:13 +0200

    add testframework for javafx

---
+++ src/test/java/de/oose/gameservice/gameservice_client/ClientApplicationTest.java
@@ -0,0 +1,12 @@
+package de.oose.gameservice.gameservice_client;
+
+import javafx.application.Application;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+class ClientApplicationTest extends ApplicationTest {
+
+    @Test
+    void start() {
+    }
+}
