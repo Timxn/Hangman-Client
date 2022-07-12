dadd59217ae162e0493102cb6b49196a73f57ee4
mayerb0 <s6bomaye@uni-bonn.de>
2022-07-12 20:27:16 +0200

    removed unused function

--- src/main/java/de/oose/gameservice/gameservice_client/util/JavaFXHelper.java
+++ src/main/java/de/oose/gameservice/gameservice_client/util/JavaFXHelper.java
@@ -11,20 +11,6 @@ import java.util.Objects;
 
 public class JavaFXHelper {
     /**
-     * @param stage The global stage that is the program
-     * @param filename The name of the page. "Test.fxml"
-     * @param width
-     * @param height
-     * @throws IOException
-     */
-    public static void enterPage(Stage stage, String filename, int width, int height) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource(filename));
-        Scene scene = new Scene(fxmlLoader.load(), width, height);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
      * @param tl Timeline that has to be stopped
      * @param stage The global stage that is the program
      * @param filename The name of the page. "Test.fxml"
